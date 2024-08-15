548f89e1261e4d078e418f7485f834d2116412a0
公益云 <168651085+gongyiyun@users.noreply.github.com>
2024-08-15 20:52:50 +0800

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt update
-sudo apt install git -y
-sudo apt install python3-pip -y
+sudo apt-get update
+sudo apt-get install git -y
+sudo apt-get install python3-pip -y
 git clone https://github.com/MatrixTM/MHDDoS.git
 cd MHDDoS
 pip install -r requirements.txt
